6d6c894df13fae241e6d6b3126c82de65c285e26
김연진 <love_homebody@naver.com>
2023-07-06 01:41:37 +0900

    router 적용

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import TodoTemplate from './components/TodoTemplate';
-import TodoHead from './components/TodoHead';
-import TodoList from './components/TodoList';
-import TodoCreate from './components/TodoCreate';
-import { TodoProvider } from './TodoContext';
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef;
-  }
-`;
+import Todo from './pages/Todo'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/About';
 
-function App() {
+const App = () => {
   return (
-    <TodoProvider>
-      <GlobalStyle />
-
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Todo />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
